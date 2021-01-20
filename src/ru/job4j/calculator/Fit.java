7d9882e05a4006e057337d98454ab51a190beebf
marinamaslova <marina_maslova_75@list.ru>
2021-01-20 15:25:01 +0300

    6. Идеальный вес [#448848]

---
+++ src/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        return (height - 100)*1.15;
+    }
+
+    public static double womanWeight(short height) {
+       return (height - 100)*1.15;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        System.out.println("Man height " + height + " is " + man);
+        height = 165;
+        double woman = Fit.womanWeight(height);
+        System.out.println("Woman height " + height + " is " + woman);
+    }
+
+}
