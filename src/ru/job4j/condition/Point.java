aae88839cb02d261edad3d67f834d859746313b5
marinamaslova <marina_maslova_75@list.ru>
2021-01-22 16:39:27 +0300

    9.2. Checkstyle [#448864]

--- src/ru/job4j/condition/Point.java
+++ src/ru/job4j/condition/Point.java
@@ -4,8 +4,8 @@ public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
         double first = x2 - x1;
         double second = y2 - y1;
-        double third = Math.pow(first,2);
-        double fourth = Math.pow(second,2);
+        double third = Math.pow(first, 2);
+        double fourth = Math.pow(second, 2);
         double fifth = third + fourth;
         return  Math.sqrt(fifth);
     }
