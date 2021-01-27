7c821089657f1069939c58458ec90c283ce49b0a
marinamaslova <marina_maslova_75@list.ru>
2021-01-27 12:59:33 +0300

    previous changes

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Marina Maslova");
+        System.out.println("27.05.1992");
+
+    }
+}
