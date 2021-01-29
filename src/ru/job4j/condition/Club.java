0c1f90d734e9f56b02e87b26f63beea510e509b6
marinamaslova <marina_maslova_75@list.ru>
2021-01-29 15:32:55 +0300

    4.3.1. Оператор && [#448801] v.2

--- src/ru/job4j/condition/Club.java
+++ src/ru/job4j/condition/Club.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        boolean result = true;
-        if (result == hasMoney || beFriend) {
+        if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
         } else {
             System.out.println("I can't.");
