1946e95da85cd0083cd37eef372d9147c647b7da
marinamaslova <marina_maslova_75@list.ru>
2021-02-04 14:04:21 +0300

    4.5. Отладка программы в IDEA [#448794] v.2

--- src/ru/job4j/condition/ThreeMax.java
+++ src/ru/job4j/condition/ThreeMax.java
@@ -1,11 +1,10 @@
 package ru.job4j.condition;
 
 public class ThreeMax {
-    @SuppressWarnings({"checkstyle:NeedBraces", "checkstyle:RightCurly"})
     public static int max(int first, int second, int third) {
         int result = first;
         if (first >= second && first > third) {
-            result = first;
+           return result;
         } else if (second > first && second >= third) {
                 result = second;
             } else {
