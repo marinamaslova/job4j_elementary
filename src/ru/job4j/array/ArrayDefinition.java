736d640610fbc68999013bf22b58701603d4db6f
marinamaslova <marina_maslova_75@list.ru>
2021-02-10 08:18:38 +0300

    6.0.2. Заполнение массива. [#448829]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surname.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Marina Maslova";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Petr Petrov";
+        names[3] = "Vasilii Vasiliev ";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 
 }
