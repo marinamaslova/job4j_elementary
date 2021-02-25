7dd07b2d6041fdecce465bdd9f40ff66d838e775
marinamaslova <marina_maslova_75@list.ru>
2021-02-25 08:55:33 +0300

    6.7.4. Выигрышные комбинации в сокобан [#448819]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,17 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int index = 0; index < 5; index++) {
+            if (board[index][index] == 'X') {
+                if (monoHorizontal(board, index) || monoVertical(board, index)) {
+                    result = true;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
 }
